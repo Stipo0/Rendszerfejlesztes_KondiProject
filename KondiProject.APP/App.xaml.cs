2a88f88207e614ec0c62e4025fbbb5e9e9bb3b29
Stipo <bencestipkovits@gmail.com>
2023-03-07 22:46:52 +0100

    init

---
+++ KondiProject.APP/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KondiProject.APP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
